84c1f73dd2272536c45ce4dac807fc3a7c04c5ce
Alex <cryptoyram@gmail.com>
2026-04-20 11:43:46 -0500

    how do i have superclasses access fields of subclasses?

--- src/Creature.java
+++ src/Creature.java
@@ -1,20 +1,22 @@
 public class Creature {
-    public float health = 1000;
     public String name;
-    public float[] dmgRange = {50, 100}  ;
-
     public String action;
 
+    public float health = 1000;
+    private float[] dmgRange = {50, 100};
+
+    private int missChance = 10;
+    private int blockChance = 20;
+    private int dodgeChance = 50;
+
+    private float blockMultiplyer = 0.5f;
 
     // Returns the damage done by the Creature
     public float genAttackPower() {
-
-        // 20% chance of missing
-        if (Rand.randomInt(0, 10) < 2) {
+        if (Rand.randomInt(0, 100) < missChance) {
             return 0;
         }
 
-        // otherwise, do damage between 10-20
         float power = Rand.randomFloat(dmgRange[0], dmgRange[1]);
         return power;
     }
@@ -31,8 +33,8 @@ public class Creature {
     public void defend(float incomingPower) {
 
         // 20% chance of reducing 50% damage taken
-        if (Rand.randomInt(0, 10) < 2) {
-            incomingPower *= 0.5f;
+        if (Rand.randomInt(0, 100) < blockChance) {
+            incomingPower *= blockMultiplyer;
             action = name + " defended and reduced damage taken to " + incomingPower;
         }
         else
@@ -42,7 +44,7 @@ public class Creature {
     }
 
     public void dodge(float incomingPower) {
-        if (Rand.randomInt(0, 10) < 5)
+        if (Rand.randomInt(0, 100) < dodgeChance)
             action = name + " dodged!";
         else {
             action = name + " attempted to dodge, but failed!";
--- src/Dog.java
+++ src/Dog.java
@@ -1,5 +1,17 @@
 public class Dog extends Creature {
 
+    public float[] dmgRange = {50, 100};
 
+    @Override
+    public float genAttackPower() {
 
+        // 10% chance of missing
+        if (Rand.randomInt(0, 10) < 1) {
+            return 0;
+        }
+
+        // otherwise, do damage between 10-20
+        float power = Rand.randomFloat(dmgRange[0], dmgRange[1]);
+        return power;
+    }
 }
